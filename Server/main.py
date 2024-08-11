8e1ae6ac120e72391dd0a5f96193c274a2b89077
VirtuallyAverage <virtuallyaverage@gmail.com>
2024-08-10 21:08:04 -0500

    Eliminate regex and address searches YAY!!!!

--- Server/main.py
+++ Server/main.py
@@ -1,12 +1,11 @@
 import asyncio
-import re
 import json
-import socket
 import time
-from pythonosc.dispatcher import Dispatcher
-from pythonosc import osc_server
-from pythonosc import udp_client
 
+from pythonosc.dispatcher import Dispatcher
+from pythonosc import udp_client, osc_server
+
+from parameters import vrc_parameters
 
 """VRCHAT & AVATAR CONFIG--------------------------------------------------------------------------------------------"""
 with open('server_config.json', 'r') as config:       #Reads Config Json and puts string into value
@@ -19,27 +18,15 @@ server_config = full_config['server_config']
 """VEST CONFIG-------------------------------------------------------------------------------------------------------"""
 vest_ip = vest_config['ip']              #Vest server IP
 vest_port = vest_config['port']     #Vest server port
-motor_limits = [x / 100 for x in vest_config['motor_limits']]
+motor_limits = vest_config['motor_limits']
 server_rate = vest_config['serv_rate'] #target a server refresh rate
 total_motors = vest_config['number_motors']    #Total number of motors!
 
 print(f"VestIP = {vest_ip}")
 print(f"VestPort = {vest_port}")
-print(f"Motor Limits: {motor_limits[0]}% to {motor_limits[1]}%")
+print(f"Motor Limits: {motor_limits["max"]*100}% to {motor_limits["min"]*100}%")
 print(f"Server Message Rate = {server_rate}hz")
 print(f"Number of motors: {total_motors}")
-
-intensity_scale = 1
-motors_enabled = True
-checks_enabled = False
-
-chest_triggered = False
-spine_triggered = False
-hip_triggered = False
-
-#if a motor or set of motors shouldn't be triggered set the corresponding value to False
-
-motor_mask = [True] * total_motors
 
 #front: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 
 #Back: 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
@@ -50,15 +37,18 @@ motor_groups = {
     'Hip':   [range(12, 15), range(27, 31)]
 }
 
-buffered_array = [float(0)] * total_motors # resets buffer for next pass
 
 """------------------------------------------------------------------------------------------------------------------"""
-
 def clear_motor_array():
     """sets the global buffered_array to zero
     """
+    global buffered_array
     buffered_array = [float(0)] * total_motors # resets buffer for next pass
 
+vrc = vrc_parameters()
+
+motor_mask = [True] * total_motors
+clear_motor_array()
 
 #init vest osc connection
 #try:
@@ -67,29 +57,21 @@ print(f'Client established on: {vest._address}:{vest._port}')
 # TODO: figure out the try and except that should be used here
 # mainly the one for when there are two servers running
 
+############################## VRC OSC HANDLERS ########################################
 
-
+motor_max = motor_limits["max"]
+motor_min = motor_limits["min"]
+motor_range = motor_max - motor_min
 def motor_handler(address, *args):
     """callback function that handles the motor osc messages.
 
     Args:
         address (string): the address that the arguments are addressed to.
     """
-    global buffered_array, intensity_scale
+    global buffered_array
     
-
-    if "Front" in address or "Back" in address :
-        motor_index = int(re.search(r'\d+', address).group())
-        motor_val = args[0]
-        
-        ScaledVal = MotorMin + ((MotorMax - MotorMin) * motor_val * intensity_scale)
-        # (range of values) -> scaled % from center -> scaled to intensity parameter -> offset by minimum
-        
-        if "Front" in address and motor_val > 0.0:
-            buffered_array[motor_index] = round(ScaledVal, 3)
-            
-        if "Back" in address and motor_val > 0.0:
-            buffered_array[motor_index + 16] = round(ScaledVal, 3)
+    scaled_val = (motor_val * motor_range + motor_min) * vrc.intensity_scale
+    buffered_array[vrc.collider_addresses[address]] = rount(scaled_val, 3)
                 
 def check_handler(address, *args):
     """callback function to handle the cross check colliders
@@ -97,43 +79,40 @@ def check_handler(address, *args):
     Args:
         address (string): _description_
     """
-    if "Spine" in address:
-        if args[0]:
-            spine_triggered = True
-        else:
-            spine_triggered = False
-    
-    elif "Hip" in address:
-        if args[0]:
-            hip_triggered = True
-        else:
-            hip_triggered = False
-            
-    elif "Chest" in address:
-        if args[0]:
-            chest_triggered = True
-        else:
-            chest_triggered = False
-            
-    else:
-        print(f"Unrecognized Parameter: {address} with args: {args}")
-            
+    vrc.handle_checks()
     update_mask()
     
+def parameter_handler(address, *args):
+    """a callback handler to deal with osc messages to the parameter address
+
+    Args:
+        address (string): the full address of the message
+    """
+    vrc.handle_params()
+    
+dispatcher = Dispatcher()
+dispatcher.map("/avatar/parameters/h/*", motor_handler)       #handles haptic data after json is jogged
+dispatcher.map("/avatar/parameters/h_param/*", parameter_handler) #handle our server parameters
+dispatcher.map("/avatar/parameters/h_Check/*", check_handler) #handle collision checks
+
+##################################### Mask Manipulation ##########################################
+
 def update_mask():
-    """Update the motor_mask variable to the latest values in the spine_triggered, hip_triggered, and chest_triggered variables
+    """Update the motor_mask variable according to new parameters
     """
     #default to all motors off
-    motor_mask = [False] * total_motors
+    set_mask(False)
      
-    if motors_enabled:
-        print("motors enabled")
+    if vrc.motors_enabled:
         
-        if(checks_enabled):
+        if(vrc.checks_enabled):
             #set each group of indices to the value of the parent collider;
-            motor_mask = set_mask_list(motor_mask, motor_groups["Spine"], spine_triggered)
-            motor_mask = set_mask_list(motor_mask, motor_groups["Hip"], hip_triggered)
-            motor_mask = set_mask_list(motor_mask, motor_groups["Chest"], chest_triggered)
+            motor_mask = set_mask_list(motor_mask, motor_groups["Spine"], vrc.spine_triggered)
+            motor_mask = set_mask_list(motor_mask, motor_groups["Hip"], vrc.hip_triggered)
+            motor_mask = set_mask_list(motor_mask, motor_groups["Chest"], vrc.chest_triggered)
+        
+        else:
+            set_mask(True)
     
     
 def set_mask_list(mask, indices:list, switch_to: bool):
@@ -151,47 +130,7 @@ def set_mask_list(mask, indices:list, switch_to: bool):
         if 0 <= index < len(mask):
             mask[index] = switch_to
     return mask
-                
-def parameter_handler(address, *args):
-    """a callback handler to deal with osc messages to the parameter address
 
-    Args:
-        address (string): the full address of the message
-    """
-    if "Enable" in address: 
-        if args[0]:
-            
-            motors_enabled = True
-            set_mask(True)
-            print("haptics On")
-            
-        else:
-            motors_enabled = False
-            set_mask(False)
-            print("Haptics Off")
-            
-    elif "Intensity" in address:
-        intensity_scale = args[0]
-        print(f"intensity set to :{intensity_scale}")
-
-    elif "Visuals" in address: #not sure why we need this here `\_('.')_/`
-        print("visualizers On")
-        
-    elif "Checks" in address:
-        if args[0]:
-            print("Checks enabled")
-            checks_enabled = True
-            #set all motors to default off
-            set_mask(False)
-        else:
-            checks_enabled = False
-            print("Checks Disabled")
-            #set motor mask to true
-            set_mask(True)
-    else:
-        print(f"Unrecognized Parameter: {address} with args: {args}")
-  
-haptic_frame_interval = 1/server_rate
        
 def apply_mask(in_list, mask):
     """Apply a mask to a list. if the boolean list for index i, list[i] = float(0.0)
@@ -215,16 +154,16 @@ def set_mask(input: bool):
     Args:
         input (bool): the bool value to set the whole list to
     """
-    global motor_mask
     motor_mask = [input] * total_motors
 
-print(vest)
+############################################ RUNTIME SETUP/MANAGEMENT ######################
+haptic_frame_interval = 1/server_rate
 #Async loop sends motor values after allowing them to be buffered for buffer_length
 async def buffer():
     """Infinite loop that asynchronously pushes the buffered_array to the vest at the serve_rate
     """
     while True:
-        global vest, buffered_array, last_time, vest_connected, motor_mask, spine_triggered, hip_triggered, chest_triggered
+        global vest, buffered_array
         
         start_time = time.time()
         
@@ -244,23 +183,22 @@ async def buffer():
             
         await asyncio.sleep(sleep_time)
 
-
-
-dispatcher = Dispatcher()
-dispatcher.map("/avatar/parameters/h/*", motor_handler)       #handles haptic data after json is jogged
-dispatcher.map("/avatar/parameters/h_param/*", parameter_handler) #handle our server parameters
-dispatcher.map("/avatar/parameters/h_Check/*", check_handler) #handle collision checks
-
 ip = "127.0.0.1"
 port = 9001
 
 async def init_main():
     server = osc_server.AsyncIOOSCUDPServer((ip, port), dispatcher, asyncio.get_event_loop())
     transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
-    
+        
     await buffer()  # Enter main loop of program
-
+        
     transport.close()  # Clean up serve endpoint
 
-
-asyncio.run(init_main())
+if __name__ == "__main__":
+    try:
+        asyncio.run(init_main())
+        
+    except KeyboardInterrupt:
+        print("Closing")
+        
+        exit()
---
+++ Server/parameters.py
@@ -0,0 +1,112 @@
+
+class vrc_parameters:
+    """a handler for all recieved vrc parameters and osc addresses
+    """
+    def __init__(self):
+        
+        #bool Parameters
+        self.motors_enabled = True
+        self.checks_enabled = False
+        self.chest_triggered = False
+        self.spine_triggered = False
+        self.hip_triggered = False
+        
+        # float parameters
+        self.intensity_scale = 1
+        
+        # address dictionaries
+        self.collider_addresses = self._build_collider_addresses()
+        self.parameter_addresses = self._build_parameter_addresses()
+        self.checks_addresses = self._build_checks_addresses()
+        
+    def handle_params(self, address, *args):
+        #TODO Try and except for parameters not in our dictionary
+        var_type, var_name = self.parameter_addresses[address]
+        
+        if var_type == type(args[0]):
+            setattr(self, var_name, args[0])
+            print(var_name, "set to:", args[0])
+        else:
+            print(f"wrong variable type at address: {address}, TYPE: {type(args[0])}, value: {args}")
+    
+    def handle_checks(self, address, *args):
+        #TODO Try and except for parameters not in our dictionary
+        var_type, var_name = self.checks_addresses[address]
+        
+        if var_type == type(args[0]):
+            setattr(self, var_name, args[0])
+            print(var_name, "set to:", args[0])
+        else:
+            print(f"wrong varible type at address: {address}, TYPE: {type(args[0])}, value: {args}")
+        
+    def _build_collider_addresses(self, 
+                                   motor_prefix = 'h', 
+                                   collider_groups = [("Front", 16), ("Back", 16)]
+                                   ):
+        """Returns a dictionary mapping addresses to their index in the motor array
+
+        Args:
+            motor_prefix (_type_): prefix past the default avatar 
+            collider_groups (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+
+        base_parameter = f"/avatar/parameters/{motor_prefix}"
+        
+        motor_colliders = {}
+        
+        # I know there has to be a linear scaling method, I don't care enough to implement it
+        colliders_seen = 0
+        for group, num_colliders in collider_groups:
+            for list_index, group_index in zip(range(colliders_seen, colliders_seen+num_colliders), range(num_colliders)):
+                motor_colliders[(f"{base_parameter}/{group} {group_index}")] = list_index
+                
+            colliders_seen += num_colliders
+            
+        return motor_colliders, colliders_seen
+    
+    def _build_parameter_addresses(self,
+                                   parameter_prefix = "h_param",
+                                   ):
+        """parameter_addresses = {
+            address: type
+        }
+        """
+        base_parameter = f"/avatar/parameters/{parameter_prefix}/"
+        
+        parameter_addresses = {
+            f'{base_parameter}Enable': (bool, 'motors_enabled'),
+            f'{base_parameter}Checks': (bool, 'checks_enabled'),
+            f'{base_parameter}Visuals': (bool, 'visuals_enabled'),
+            f'{base_parameter}Intensity': (float, 'intensity_scale'),
+        }
+        
+        return parameter_addresses
+    
+    def _build_checks_addresses(self,
+                                   parameter_prefix = "h_Checks",
+                                   ):
+        """parameter_addresses = {
+            address: (type, name)
+        }
+        """
+        base_parameter = f"/avatar/parameters/{parameter_prefix}/"
+        
+        checks_addresses = {
+            f'{base_parameter}Chest': (bool, 'chest_triggered'),
+            f'{base_parameter}Spine': (bool, 'spine_triggered'),
+            f'{base_parameter}Hip': (bool, 'hip_triggered'),
+        }
+        
+        return checks_addresses
+
+    
+if __name__ == "__main__":
+    parameters = vrc_parameters()
+    print(parameters.collider_addresses)
+    print(parameters.parameter_addresses)
+    print(parameters.checks_addresses)
+    
+
